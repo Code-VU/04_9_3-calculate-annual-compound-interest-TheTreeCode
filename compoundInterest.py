cf3a8ff8eac925a3731fa5302a9cde38318bd135
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-17 10:56:46 -0600

    Trying to figure out how to get the fuction to call two more times with --- before the second two

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,12 +1,41 @@
-def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+def getACompoundInterest():
+    while True:
+        try:
+            client_one_principal = float(input("Principle (amount): "))
+            break
+        except:
+            print("Please enter only numeric values without any text or special charachters.")
+            continue
+    while True:
+        try:
+            client_one_time = float(input("Time:               "))
+            break
+        except:
+            print("Please enter only numeric values without any text or special charachters.")
+            continue
+    while True:
+        try:
+            client_one_rate = float(input("Rate:               "))
+            break
+        except:
+            print("Please enter only numeric values without any text or special charachters.")
+            continue
+    return [client_one_principal, client_one_time, client_one_rate]
 
+def calculateCompoundInterest():
+    #Amount=PrincipalAmount(1+InterestRate/100)**Time
+    #CompoundInterest=Amount-PrincipalAmount
+    principal_time_rate_LIST = getACompoundInterest()
+    #The fuction being passed to the principal_time... variable returns a list with [principal, time, rate].
+    a = principal_time_rate_LIST[0]*(1+principal_time_rate_LIST[2]/100)**principal_time_rate_LIST[1]
+    ci = round(a - principal_time_rate_LIST[0],2)
+    print("Compound Interest: "+str(ci))
+
+calculateCompoundInterest()
+print("---")
+calculateCompoundInterest()
+print("---")
+calculateCompoundInterest()
     # end assignment
 
 ## If you want to test locally run > python compoundInterest.py
