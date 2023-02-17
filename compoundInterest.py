90e0d1401d89096db4efd15a60f7b4a2cbc3e318
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-17 11:16:53 -0600

    delete lines that called the function two more times

--- compoundInterest.py
+++ compoundInterest.py
@@ -23,22 +23,13 @@ def getACompoundInterest():
     return [client_one_principal, client_one_time, client_one_rate]
 
 def calculateCompoundInterest():
-    #Amount=PrincipalAmount(1+InterestRate/100)**Time
-    #CompoundInterest=Amount-PrincipalAmount
     principal_time_rate_LIST = getACompoundInterest()
-    #The fuction being passed to the principal_time... variable returns a list with [principal, time, rate].
+    #The fuction being passed to the "principal_time_rate_LIST" variable returns a list with [principal, time, rate].
     a = principal_time_rate_LIST[0]*(1+principal_time_rate_LIST[2]/100)**principal_time_rate_LIST[1]
     ci = round(a - principal_time_rate_LIST[0],2)
     print("Compound Interest: "+str(ci))
 
-calculateCompoundInterest()
-print("---")
-calculateCompoundInterest()
-print("---")
-calculateCompoundInterest()
-    # end assignment
 
-## If you want to test locally run > python compoundInterest.py
 
 if __name__ == "__main__":
     calculateCompoundInterest()
