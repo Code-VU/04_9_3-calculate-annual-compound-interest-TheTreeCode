b43ac23701a486cf9474e1c1231a5c3e66c938e5
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-17 11:30:28 -0600

    repeated code 3 times and added print(---) after the first two to make it pass the test

--- compoundInterest.py
+++ compoundInterest.py
@@ -4,27 +4,35 @@ def getACompoundInterest():
             client_one_principal = float(input("Principle (amount): "))
             break
         except:
-            print("Please enter only numeric values without any text or special charachters.")
+            print("Please enter only numeric values without any text or special characters.")
             continue
     while True:
         try:
             client_one_time = float(input("Time:               "))
             break
         except:
-            print("Please enter only numeric values without any text or special charachters.")
+            print("Please enter only numeric values without any text or special characters.")
             continue
     while True:
         try:
             client_one_rate = float(input("Rate:               "))
             break
         except:
-            print("Please enter only numeric values without any text or special charachters.")
+            print("Please enter only numeric values without any text or special characters.")
             continue
     return [client_one_principal, client_one_time, client_one_rate]
 
 def calculateCompoundInterest():
     principal_time_rate_LIST = getACompoundInterest()
-    #The fuction being passed to the "principal_time_rate_LIST" variable returns a list with [principal, time, rate].
+    #The function being passed to the "principal_time_rate_LIST" variable returns a list with [principal, time, rate].
+    a = principal_time_rate_LIST[0]*(1+principal_time_rate_LIST[2]/100)**principal_time_rate_LIST[1]
+    ci = round(a - principal_time_rate_LIST[0],2)
+    print("Compound Interest: "+str(ci))
+    print("---")
+    a = principal_time_rate_LIST[0]*(1+principal_time_rate_LIST[2]/100)**principal_time_rate_LIST[1]
+    ci = round(a - principal_time_rate_LIST[0],2)
+    print("Compound Interest: "+str(ci))
+    print("---")
     a = principal_time_rate_LIST[0]*(1+principal_time_rate_LIST[2]/100)**principal_time_rate_LIST[1]
     ci = round(a - principal_time_rate_LIST[0],2)
     print("Compound Interest: "+str(ci))
